dfea8d1f09d5f4f6350b5cc5b9587033a8e4bbe6
Suneesh <suneesh.anand@gmail.com>
2017-07-14 01:53:37 +0000

    Openhab Kodi Volume Integration - But disabled

--- VolumeDown.sh
+++ VolumeDown.sh
@@ -14,4 +14,22 @@ fi
 
 echo $i > "a.vol"
 
-$AMIXER_CMD -q -c0 -Dhw:sndrpiwsp cset numid=160 $i
+$AMIXER_CMD -q -c 0 -Dhw:sndrpiwsp cset numid=160 $i
+
+
+#factor=`expr 23 / 10`
+
+#echo factor=$factor
+
+#kodi_volume=`expr $i / $factor`
+
+#echo $kodi_volume
+
+#datastring="-d \"$kodi_volume\""
+
+#echo $datastring
+
+#eval "curl -X POST --header \"Content-Type: text/plain\" --header \"Accept: application/json\"  ${datastring} \"http://192.168.1.10:8080/rest/items/KodiMediacenter_Volume\" "
+
+
+
